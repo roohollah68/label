a7af266bd7adafb4e5397e5488c730e511f6a4f9
roohollah68 <rshirkhany@gmail.com>
2021-02-01 15:19:56 +0330

    another update

--- resources/views/addForm.blade.php
+++ resources/views/addForm.blade.php
@@ -6,38 +6,48 @@
 
 
 @section('content')
+
     <form action="" method="post">
         @csrf
-        <div class="form-group">
-            <label>نام و نام خانوادگی:</label>
-            <input type="text" name="name" placeholder="نام و نام خانوادگی را وارد کنید">
-        </div>
+        <div class="row">
+            <div class="col-sm-6">
 
-        <div class="form-group">
-            <label>شماره تماس:</label>
-            <input type="tel" name="phone" placeholder="شماره موبایل را وارد کنید">
-        </div>
+                <div class="form-group">
+                    <label for="name">نام و نام خانوادگی:</label>
+                    <input type="text" id="name" class="form-control" name="name"
+                           placeholder="نام و نام خانوادگی را وارد کنید">
+                </div>
 
-        <div class="form-group">
-            <label>آدرس:</label>
-            <textarea name="address" rows="3"></textarea>
-        </div>
+                <div class="form-group">
+                    <label for="phone">شماره تماس:</label>
+                    <input type="tel" id="phone" class="form-control" name="phone"
+                           placeholder="شماره موبایل را وارد کنید">
+                </div>
 
-        <div class="form-group">
-            <label>کد پستی:</label>
-            <input type="text" name="zip_code" placeholder="کد پستی را وارد کنید">
-        </div>
+                <div class="form-group">
+                    <label for="address">آدرس:</label>
+                    <textarea id="address" class="form-control" name="address" rows="3"></textarea>
+                </div>
 
-        <div class="form-group">
-            <label>سفارشات:</label>
-            <textarea name="orders" rows="3"></textarea>
-        </div>
+                <div class="form-group">
+                    <label for="zip_code">کد پستی:</label>
+                    <input type="text" id="zip_code" class="form-control" name="zip_code"
+                           placeholder="کد پستی را وارد کنید">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="orders">سفارشات:</label>
+                    <textarea id="orders" class="form-control" name="orders" rows="3"></textarea>
+                </div>
 
-        <div class="form-group">
-            <label>توضیحات:</label>
-            <textarea name="desc" rows="3"></textarea>
+                <div class="form-group">
+                    <label for="desc">توضیحات:</label>
+                    <textarea id="desc" class="form-control" name="desc" rows="3"></textarea>
+                </div>
+                <input type="submit" class="btn btn-success" value="ذخیره">
+                <input type="reset" class="btn btn-danger" value="پاک کردن">
+            </div>
         </div>
-        <input type="submit" value="submit">
-
     </form>
 @endsection
--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,3 +1,10 @@
-<a href="/">خانه</a><br>
-<a href="add_order">ایجاد سفارش</a><br>
-<a href="list_orders">مشاهده سفارشات</a><br>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="/">خانه</a>
+            <a class="nav-item nav-link" href="add_order">ایجاد سفارش</a>
+            <a class="nav-item nav-link" href="list_orders">مشاهده سفارشات</a>
+        </div>
+    </div>
+</nav>
