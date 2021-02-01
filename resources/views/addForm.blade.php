2c5e2dd08bc003aa13121167b4be2b517987d9ea
roohollah68 <rshirkhany@gmail.com>
2021-02-01 16:21:17 +0330

    another update

--- resources/views/addForm.blade.php
+++ resources/views/addForm.blade.php
@@ -10,44 +10,31 @@
     <form action="" method="post">
         @csrf
         <div class="row">
-            <div class="col-sm-6">
+            @foreach([
+                ['name' , 'text' , 'نام و نام خانوادگی' , 'input' , 'required'],
+                ['phone' , 'tel' , 'شماره تماس' , 'input', ''],
+                ['address' , 'text' , 'آدرس' , 'textarea', 'required'],
+                ['zip_code' , 'text' , 'کد پستی' , 'input', ''],
+                ['orders' , 'text' , 'سفارشات' , 'textarea', 'required'],
+                ['desc' , 'text' , 'توضیحات' , 'textarea', ''],
 
-                <div class="form-group">
-                    <label for="name">نام و نام خانوادگی:</label>
-                    <input type="text" id="name" class="form-control" name="name"
-                           placeholder="نام و نام خانوادگی را وارد کنید">
+            ] as $arr)
+                <div class="col-sm-6">
+                    <div class="form-group input-group {{$arr[4]}}">
+                        <div class="input-group-append">
+                            <label for="{{$arr[0]}}" class="input-group-text">{{$arr[2]}}:</label>
+                        </div>
+                        <{{$arr[3]}} type="{{$arr[1]}}" id="{{$arr[0]}}" class="form-control" name="{{$arr[0]}}" rows="2" {{$arr[4]}}></{{$arr[3]}}>
+{{--                        <div class="input-group-prepend">--}}
+{{--                            <span class="btn btn-outline-secondary fa fa-paste" onclick="$('#{{$arr[0]}}').val(await navigator.clipboard.readText())"></span>--}}
+{{--                        </div>--}}
+                    </div>
                 </div>
-
-                <div class="form-group">
-                    <label for="phone">شماره تماس:</label>
-                    <input type="tel" id="phone" class="form-control" name="phone"
-                           placeholder="شماره موبایل را وارد کنید">
-                </div>
-
-                <div class="form-group">
-                    <label for="address">آدرس:</label>
-                    <textarea id="address" class="form-control" name="address" rows="3"></textarea>
-                </div>
-
-                <div class="form-group">
-                    <label for="zip_code">کد پستی:</label>
-                    <input type="text" id="zip_code" class="form-control" name="zip_code"
-                           placeholder="کد پستی را وارد کنید">
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <label for="orders">سفارشات:</label>
-                    <textarea id="orders" class="form-control" name="orders" rows="3"></textarea>
-                </div>
-
-                <div class="form-group">
-                    <label for="desc">توضیحات:</label>
-                    <textarea id="desc" class="form-control" name="desc" rows="3"></textarea>
-                </div>
-                <input type="submit" class="btn btn-success" value="ذخیره">
-                <input type="reset" class="btn btn-danger" value="پاک کردن">
-            </div>
+        @endforeach
         </div>
+        <input type="submit" class="btn btn-success" value="ذخیره">&nbsp;
+        <input type="reset" class="btn btn-danger" value="پاک کردن">
+
+
     </form>
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,7 +7,13 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+          integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+          crossorigin="anonymous"/>
+
     <link rel="stylesheet" href="{{mix('css/style.css')}}">
 </head>
 
