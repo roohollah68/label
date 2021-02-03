3f1445b9b0de37d2879b0f763e25baad79579770
roohollah68 <rshirkhany@gmail.com>
2021-02-03 14:54:36 +0330

    another update

--- public/js/orders.js
+++ public/js/orders.js
@@ -29,9 +29,9 @@ function prepare_data() {
             row.desc,
             `<i class="fa fa-eye btn btn-info" onclick="view_order(${row.id})"></i>`,
             ` <i class="fa fa-trash-alt btn btn-danger" onclick="delete_order(${row.id})" title="حذف سفارش" ></i>` +
-            (deleted ?`
+            (deleted ? `
                     <i class="fa fa-trash-restore btn btn-primary" onclick="restore_order(${row.id})" title="بازگردانی"></i>
-            `:`
+            ` : `
                     <a class="fa fa-edit btn btn-primary" href="edit_order/${row.id}" title="ویرایش سفارش"></a>`
             )
             +
@@ -55,23 +55,69 @@ function create_table(data) {
             {title: "عملیات"},
         ],
         data: data,
+        language: {
+            "decimal": "",
+            "emptyTable": "هیچ سفارشی موجود نیست",
+            "info": "نمایش _START_ تا _END_ از _TOTAL_ مورد",
+            "infoEmpty": "نمایش  0 تا 0 از 0 مورد",
+            "infoFiltered": "(filtered from _MAX_ total entries)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "نمایش _MENU_ مورد",
+            "loadingRecords": "در حال بارگذاری...",
+            "processing": "در حال پردازش...",
+            "search": "جستجو:",
+            "zeroRecords": "هیچ مورد منطبقی یافت نشد",
+            "paginate": {
+                "first": "اولین",
+                "last": "آخرین",
+                "next": "بعدی",
+                "previous": "قبلی"
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            }
+        }
     });
-    $('.main_check').attr('checked',false);
+    $('.main_check').attr('checked', false);
 }
 
 function delete_order(id) {
-    $.post('delete_order/' + id, {_token: token, deleted: deleted})
-        .done(res => {
-            $.notify(res, 'info');
-            get_data();
-        })
+    deleted ?
+        confirm("برای همیشه حذف شود؟") ?
+            $.post('delete_order/' + id, {_token: token, deleted: deleted})
+                .done(res => {
+                    $.notify(res, 'info');
+                    get_data();
+                })
+            : ''
+        :
+        ($.post('delete_order/' + id, {_token: token, deleted: deleted})
+            .done(res => {
+                $.notify(res, 'info');
+                get_data();
+            }))
 }
 
 function delete_orders() {
-    $.post('delete_orders', {_token: token, deleted: deleted, ids: ids})
-        .done(res => {
-            get_data();
-        })
+    if (ids.length === 0) {
+        $.notify('ابتدا باید سفارشات مورد نظر را انتخاب کنید', 'error')
+        return
+    }
+    deleted ?
+        confirm("برای همیشه حذف شوند؟") ?
+            $.post('delete_orders', {_token: token, deleted: deleted, ids: ids})
+                .done(res => {
+                    get_data();
+                })
+            :
+            ""
+        :
+        $.post('delete_orders', {_token: token, deleted: deleted, ids: ids})
+            .done(res => {
+                get_data();
+            })
 }
 
 function restore_order(id) {
@@ -142,19 +188,21 @@ function generatePDF(id) {
         }
     })
     let dialog = `
-    <span>نام و نام خانوادگی </span>: <b>${row.name}</b> <br>
+<div class="printed">
+    <span>نام و نام خانوادگی </span>: <b>${fix_persian(row.name)}</b> <br>
     <span>شماره تماس </span>: <b>${row.phone}</b> <br>
-    <span>آدرس </span>: <b>${row.address}</b> <br>
+    <span>آدرس </span>: <b>${fix_persian(row.address)}</b> <br>
     <span>کد پستی </span>: <b>${row.zip_code}</b> <br>
-    <span>سفارشات </span>: <b>${row.orders}</b> <br>
-
+    <span>سفارشات </span>: <b>${fix_persian(row.orders)}</b> <br>
+    <span>توضیحات </span>: <b>${fix_persian(row.desc)}</b>
+</div>
     `;
+    console.log(dialog);
     let opt = {
-        margin: 0.2,
-        image: {type:'jpeg',quality : 1},
-        filename:     row.name +'_'+row.id +'.pdf',
-        // pagebreak: {  after: '.breakhere' },
-        jsPDF:        { format: [7.12, 2.18],unit:'in',orientation :'l' }
+        margin: 0.6,
+        image: {type: 'jpeg', quality: 1},
+        filename: row.name + '_' + row.id + '.pdf',
+        jsPDF: {format: [30, 15], unit: 'cm', orientation: 'l'}
     };
     html2pdf()
         .set(opt)
@@ -163,13 +211,12 @@ function generatePDF(id) {
 }
 
 function generatePDFs() {
-    if(ids.length ===0)
-    {
-        $.notify( 'ابتدا باید سفارشات مورد نظر را انتخاب کنید','error')
+    if (ids.length === 0) {
+        $.notify('ابتدا باید سفارشات مورد نظر را انتخاب کنید', 'error')
         return
     }
     let dialog = [];
-    ids.forEach(id=>{
+    ids.forEach(id => {
         let row
         orders.forEach(order => {
             if (order.id == id) {
@@ -178,22 +225,34 @@ function generatePDFs() {
             }
         })
         dialog.push(`
-    <span>نام و نام خانوادگی </span>: <b>${row.name}</b> <br>
+<div class="printed">
+    <span>نام و نام خانوادگی </span>: <b>${fix_persian(row.name)}</b> <br>
     <span>شماره تماس </span>: <b>${row.phone}</b> <br>
-    <span>آدرس </span>: <b>${row.address}</b> <br>
+    <span>آدرس </span>: <b>${fix_persian(row.address)}</b> <br>
     <span>کد پستی </span>: <b>${row.zip_code}</b> <br>
-    <span>سفارشات </span>: <b>${row.orders}</b>
-    `) ;})
-    dialog=dialog.join('<br class="breakhere">')
+    <span>سفارشات </span>: <b>${fix_persian(row.orders)}</b> <br>
+    <span>توضیحات </span>: <b>${fix_persian(row.desc)}</b>
+</div>
+    `);
+    })
+    dialog = dialog.join('<br class="breakhere">')
     let opt = {
-        margin: 0.2,
-        image: {type:'jpeg',quality : 1},
-        filename:     'سفازشات.pdf',
-        pagebreak: {  after: '.breakhere' },
-        jsPDF:        { format: [7.12, 2.18],unit:'in',orientation :'l' }
+        margin: 0.6,
+        image: {type: 'jpeg', quality: 1},
+        filename: 'سفازشات.pdf',
+        pagebreak: {after: '.breakhere'},
+        jsPDF: {format: [30, 15], unit: 'cm', orientation: 'l'}
     };
     html2pdf()
         .set(opt)
         .from(dialog)
         .save();
+}
+
+function fix_persian(text) {
+    let symbols = [",", ".", "-", "_", "#", "@", "(", ")","{","}","[","]","،","$",];
+    symbols.forEach(symbol=>{
+        text = text.split(symbol).join("</b>"+symbol+"<b>")
+    })
+    return text
 }
