3c009fd4d99b185c1b1bf7a95ccde916a8685db2
roohollah68 <rshirkhany@gmail.com>
2021-02-03 13:26:17 +0330

    another update

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,10 +1,22 @@
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link" href="/">خانه</a>
-            <a class="nav-item nav-link" href="{{route('newOrder')}}">ایجاد سفارش</a>
-            <a class="nav-item nav-link" href="{{route('listOrders')}}">مشاهده سفارشات</a>
-        </div>
-    </div>
-</nav>
+{{--<nav class="navbar navbar-expand-lg navbar-light bg-light">--}}
+{{--    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">--}}
+{{--        <div class="navbar-nav">--}}
+{{--            <a class="nav-item nav-link" href="/">خانه</a>--}}
+{{--            <a class="nav-item nav-link" href="{{route('newOrder')}}">ایجاد سفارش</a>--}}
+{{--            <a class="nav-item nav-link" href="{{route('listOrders')}}">مشاهده سفارشات</a>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</nav>--}}
+
+<ul class="nav">
+    <li class="nav-item">
+        <a class="nav-item nav-link" href="/">خانه</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-item nav-link" href="{{route('newOrder')}}">ایجاد سفارش</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-item nav-link" href="{{route('listOrders')}}">مشاهده سفارشات</a>
+    </li>
+</ul>
