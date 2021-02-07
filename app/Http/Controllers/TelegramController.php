75249fc2ce0cf4e21e2d86a22d9ec749e27cb3af
roohollah68 <rshirkhany@gmail.com>
2021-02-07 13:14:01 +0330

    another update

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class TelegramController extends Controller
 {
     public function receive(Request $request)
     {
-        file_put_contents('res.txt',$request->url());
+        Storage::disk('public')->put('res.txt', "hi");
+//        file_put_contents('res.txt',$request->url());
     }
 }
