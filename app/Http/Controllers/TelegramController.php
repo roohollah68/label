b68dd3406a684a96198bace2d6501a68f5434b91
roohollah68 <rshirkhany@gmail.com>
2021-02-07 14:11:55 +0330

    another update

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -10,7 +10,7 @@ class TelegramController extends Controller
 {
     public function receive(Request $request)
     {
-        Storage::disk('public')->put('res.txt', $request);
+        Storage::disk('public')->put('res.txt', json_encode($request->all()));
 //        file_put_contents('res.txt',$request->url());
     }
 }
