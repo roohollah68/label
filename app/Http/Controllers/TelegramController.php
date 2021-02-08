36b2a6a3f39594524c335187120d4947df02718a
roohollah68 <rshirkhany@gmail.com>
2021-02-08 17:08:04 +0330

    another update

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -43,7 +43,7 @@ class TelegramController extends Controller
                 $file_id = end($this->req->message->photo)->file_id;
                 $caption = "برای ثبت فاکتور مربوط به این رسید روی لینک زیر کلیک کنید";
                 $url = env('APP_URL')."new-order-receipt/{$user->id}/{$user->password}/{$file_id}";
-                $keyboard = new RKM(Keyboard::register_user($url,"ثبت فاکتور مربوط به این رسید"));
+                $keyboard = new IKM(Keyboard::register_user($url,"ثبت فاکتور مربوط به این رسید"));
                 $this->bot->sendPhoto($this->chat_id,$file_id,$caption,$this->req->message->message_id,$keyboard);
             }
 
